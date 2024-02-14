906006356c3af7f4983a1c7ffb132aa9651d2033
p9tno <plahotin_s@mail.ru>
2024-02-14 20:42:17 +0300

    work a map

--- js/map.js
+++ js/map.js
@@ -10,12 +10,9 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     async function initMap() {
-        //@ts-ignore
         const { Map } = await google.maps.importLibrary('maps');
-        // 34.05337154967566, -118.36230308012492
 
         const bounds = new google.maps.LatLngBounds();
-
         mapMarkers.forEach((marker) => {
             bounds.extend(new google.maps.LatLng(marker.position.lat, marker.position.lng));
         });
@@ -26,19 +23,17 @@ document.addEventListener('DOMContentLoaded', () => {
             disableDefaultUI: true,
             mapId: 'e87a5ee68e47b684',
         });
-        // console.log(map);
-        // console.log(bounds);
 
         map.fitBounds(bounds);
 
 
 
-        mapMarkers.forEach(({ title, position, place, text, link, images }, i) => {
+        mapMarkers.forEach(({ title, position, place, text, link, images, icon }, i) => {
             // console.log(title, position);
 
             const addMarker = () => {
                 const marker = new google.maps.Marker({
-                    icon: '../../img/marker.svg',
+                    icon: icon,
                     position,
                     animation: google.maps.Animation.DROP,
                     map,
@@ -47,33 +42,59 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
 
                 marker.addListener('click', (e) => {
-                    // if (!mark.classList.contains('open')) {
-                    //
-                    //     markContent.innerHTML = setMarkerContent(title, place, text, link, images);
-                    //     mark.classList.add('open');
-                    // }
-
-                    mark.classList.remove('open');
-                    markContent.innerHTML = setMarkerContent(title, place, text, link, images);
-                    initSlider();
-
-                    setTimeout(() => {
+                    if (!mark.classList.contains('open')) {
+                        markContent.innerHTML = setMarkerContent(title, place, text, link, images);
+                        initSlider();
                         mark.classList.add('open');
-
-                    }, 100);
-
+                    } else {
+                        mark.classList.remove('open');
+                        setTimeout(() => {
+                            markContent.innerHTML = setMarkerContent(title, place, text, link, images);
+                            initSlider();
+                            mark.classList.add('open');
+                        }, 600);
+                    }
                 });
             };
 
             setTimeout(addMarker, 500 * i);
         });
 
-
-
-
     }
 
-    initMap();
+    function onVisible( selector, callback, repeat = false ) {
+
+        let options = {
+            threshold: [ 0.5 ]
+        };
+        let observer = new IntersectionObserver( onEntry, options );
+        let elements = document.querySelectorAll( selector );
+
+        for ( let elm of elements ) {
+            observer.observe( elm );
+        }
+
+        function onEntry( entry ) {
+            entry.forEach( change => {
+                let elem = change.target;
+                // console.log(change);
+                // console.log(elem.innerHTML);
+                if ( change.isIntersecting ) {
+                    if ( !elem.classList.contains( 'show' ) || repeat ) {
+                        elem.classList.add( 'show' );
+                        callback( elem );
+                    }
+                }
+            } );
+        }
+    }
+
+    onVisible( '.construction', function ( e ) {
+        console.log('initMap');
+        initMap();
+    } );
+
+    // initMap();
 
     // if(map) {
     //     map.setZoom(1);
@@ -83,62 +104,61 @@ document.addEventListener('DOMContentLoaded', () => {
         let slides = '';
         images.forEach((img) => {
             slides += `<div class="swiper-slide">
-				<img
-					src="${img}"
-					alt=""
-				/>
+                <div class="mark__img img">
+                    <img
+                        src="${img}"
+                        alt=""
+                    />
+                </div>
 			</div>`;
         });
 
-        return `<div class="mark__thumb">
+        return `<div class="mark__slider">
 
-                <div class="swiper preview_markSlider_js">
+                <div class="swiper markSlider_js">
                     <div class="swiper-wrapper">
                         ${slides}
                     </div>
                 </div>
 
-
-
-				<div class="markSlider__action">
-                    <div class="swiper-control">
+				<div class="mark__control">
+                    <div class="swiper-control dark">
                         <i class="swiper-arrow icon_arrow_left"></i>
                         <div class="swiper-pagination"></div>
                         <i class="swiper-arrow icon_arrow_right"></i>
                     </div>
 				</div>
 			</div>
+
 			<div class="mark__body">
 				<div class="mark__title">${title}</div>
 				<div class="mark__place">${place}</div>
 				<div class="mark__text">${text}</div>
 			</div>
 			<div class="mark__foot">
-				<a
-					href="${link}"
-					class="mark__link"
-					>Do you need a similar kitchen? contact us</a
-				>
+				<a class="btn" href="${link}">
+                  <span>Do you need a similar kitchen? contact us</span></a>
 			</div>
 		`;
     }
 });
 
-
 function initSlider() {
-    console.log('initSlider');
-    const markSlider = new Swiper('.preview_markSlider_js', {
+    const markSlider = new Swiper('.markSlider_js', {
         slidesPerView: 1,
         spaceBetween: 0,
-        speed: 500,
+        speed: 2000,
         loop: true,
+        autoplay: {
+          delay: 5000,
+        },
 
         navigation: {
             nextEl: '.icon_arrow_right',
             prevEl: '.icon_arrow_left',
         },
         pagination: {
-            el: ".markSlider__action .swiper-pagination",
+            el: ".mark__control .swiper-pagination",
             type: "fraction",
         },
 
--- js/slider.js
+++ js/slider.js
@@ -86,341 +86,47 @@ $(document).ready(function() {
 
 
 
-    const ourhouses = new Swiper('.ourhouses-swiper-js', {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        speed: 500,
-        loop: true,
-        // autoplay: {
-        //   delay: 5000,
-        // },
-
-        navigation: {
-            nextEl: '.ourhouses__arrows .icon_arrow_right',
-            prevEl: '.ourhouses__arrows .icon_arrow_left',
-        },
-        pagination: {
-            el: '.ourhouses__dotted',
-            clickable: true,
-        },
-        initialSlide: 0,
 
 
-
-        breakpoints: {
-            768: {
-                spaceBetween: 27,
-                slidesPerView: 4,
-            },
-
-        }
-    });
-
-    const data = new Swiper('.data-swiper-js', {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        speed: 500,
-        // loop: true,
-        // autoplay: {
-        //   delay: 5000,
-        // },
-
-        navigation: {
-            nextEl: '.icon_arrow_right',
-            prevEl: '.icon_arrow_left',
-        },
-        pagination: {
-            el: '.data__dotted',
-        },
-
-        breakpoints: {
-            768: {
-                spaceBetween: 80,
-                slidesPerView: 3,
-            },
-
-        }
-    });
-
-    const teamImgSlider = new Swiper('.teamImgSlider-swiper-js', {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        speed: 500,
-        loop: true,
-        // autoplay: {
-        //   delay: 5000,
-        // },
-
-        navigation: {
-            nextEl: '.icon_arrow_right',
-            prevEl: '.icon_arrow_left',
-        },
-        pagination: {
-            el: '.teamImgSlider__dotted',
-        },
-
-        breakpoints: {
-            768: {
-                spaceBetween: 74,
-                slidesPerView: 3,
-            },
-
-        }
-    });
-
-    const blog = new Swiper('.blog-swiper-js', {
-        slidesPerView: 1,
-        speed: 500,
-        loop: true,
-        // autoplay: {
-        //   delay: 5000,
-        // },
-
-        navigation: {
-            nextEl: '.icon_arrow_right',
-            prevEl: '.icon_arrow_left',
-        },
-        pagination: {
-            el: '.blog__dotted',
-            // clickable: true,
-        },
-
-        spaceBetween: 10,
-
-        breakpoints: {
-            768: {
-                spaceBetween: 42,
-                slidesPerView: 2,
-            },
-
-        }
-    });
-
-    const singleSlider = new Swiper('.single-swiper-js', {
-        slidesPerView: 1,
-        speed: 500,
-        // loop: true,
-        // autoplay: {
-        //   delay: 5000,
-        // },
-
-        navigation: {
-            nextEl: '.icon_arrow_right',
-            prevEl: '.icon_arrow_left',
-        },
-        pagination: {
-            el: '.singleSlider__dotted',
-            clickable: true,
-        },
-
-    });
-
-    const largeSlider = new Swiper('.largeSlider-swiper-js', {
-        slidesPerView: 1,
-        speed: 500,
-        // loop: true,
-        // autoplay: {
-        //   delay: 5000,
-        // },
-        loop: true,
-        effect: "fade",
-        fadeEffect: {
-          crossFade: true
-        },
-
-        navigation: {
-            nextEl: '.icon_arrow_right',
-            prevEl: '.icon_arrow_left',
-        },
-        pagination: {
-            el: '.largeSlider__dotted',
-            clickable: true,
-        },
-
-    });
-
-    // BEGIN TEAM
-    const teamSlider = new Swiper('.team-swiper-js', {
-        slidesPerView: 2,
-        speed: 500,
-        loop: true,
-        // centeredSlides: true,
-        freeMode: true,
-        watchSlidesProgress: true,
-
-        breakpoints: {
-            768: {
-                slidesPerView: 4,
-            },
-        }
-    });
-
-
-    const teamPreview = new Swiper('.teamPreview-swiper-js', {
-        slidesPerView: 1,
-        speed: 500,
-        loop: true,
-        effect: "fade",
-        fadeEffect: {
-          crossFade: true
-        },
-
-        allowTouchMove: false,
-
-        navigation: {
-            nextEl: '.icon_arrow_right',
-            prevEl: '.icon_arrow_left',
-        },
-        pagination: {
-            el: '.teamSlider__dotted',
-            clickable: true,
-        },
-
-        thumbs: {
-            swiper: teamSlider,
-        },
-
-        slideToClickedSlide: true,
-        autoHeight: true,
-
-    });
-    teamPreview.on('slideChange', function (e) {
-        // console.log('slide changed');
-        // console.log(e.activeIndex);
-    });
-
-    // END TEAM
-
-    const videoWork = new Swiper('.videoWork-swiper-js', {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        speed: 500,
-        loop: true,
-        // autoplay: {
-        //   delay: 5000,
-        // },
-
-        navigation: {
-            nextEl: '.icon_arrow_right',
-            prevEl: '.icon_arrow_left',
-        },
-        pagination: {
-            el: '.videoWork__dotted',
-            clickable: true,
-        },
-
-        breakpoints: {
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 56,
-            },
-        }
-
-    });
-
-    const gifts = new Swiper('.gifts-swiper-js', {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        speed: 500,
-        loop: true,
-        // autoplay: {
-        //   delay: 5000,
-        // },
-
-        navigation: {
-            nextEl: '.icon_arrow_right',
-            prevEl: '.icon_arrow_left',
-        },
-        pagination: {
-            el: '.gifts__dotted',
-            clickable: true,
-        },
-
-        breakpoints: {
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 40,
-            },
-        }
-
-    });
-
-    const stages = new Swiper('.stages-swiper-js', {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        speed: 500,
-        loop: true,
-        // autoplay: {
-        //   delay: 5000,
-        // },
-
-        navigation: {
-            nextEl: '.icon_arrow_right',
-            prevEl: '.icon_arrow_left',
-        },
-        pagination: {
-            el: '.stages__dotted',
-            clickable: true,
-        },
-
-        breakpoints: {
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 60,
-            },
-        }
-
-    });
-
-    const logos = new Swiper('.logos-swiper-js', {
-        slidesPerView: 3,
-        speed: 500,
-        autoplay: {
-          delay: 5000,
-        },
-
-        breakpoints: {
-            768: {
-                slidesPerView: 8,
-            },
-        }
-    });
-
-    // logos.on('slideChange', function () {
-    //     console.log('slide changed');
+    // const teamPreview = new Swiper('.teamPreview-swiper-js', {
+    //     slidesPerView: 1,
+    //     speed: 500,
+    //     loop: true,
+    //     effect: "fade",
+    //     fadeEffect: {
+    //       crossFade: true
+    //     },
     //
-    //     let leftSlide = logos.activeIndex;
-    //     let rightSlide = logos.activeIndex + 2;
-    //     leftSlide.add('left_slide');
-    //     rightSlide.add('right_slide');
+    //     allowTouchMove: false,
     //
-    //     console.log(leftSlide);
-    //     console.log(rightSlide);
-    //     console.log(logos.activeIndex);
-    //     console.log(logos.$el);
+    //     navigation: {
+    //         nextEl: '.icon_arrow_right',
+    //         prevEl: '.icon_arrow_left',
+    //     },
+    //     pagination: {
+    //         el: '.teamSlider__dotted',
+    //         clickable: true,
+    //     },
+    //
+    //     thumbs: {
+    //         swiper: teamSlider,
+    //     },
+    //
+    //     slideToClickedSlide: true,
+    //     autoHeight: true,
+    //
+    // });
+    // teamPreview.on('slideChange', function (e) {
+    //     // console.log('slide changed');
+    //     // console.log(e.activeIndex);
     // });
 
-    // let activeSlide = swiper.activeIndex;
 
 
 
 
-    const otherProjects = new Swiper(".otherProjects-swiper-js", {
-        slidesPerView: 1,
-        allowTouchMove: false,
-        loop: true,
-
-        navigation: {
-            nextEl: '.icon_arrow_right',
-            prevEl: '.icon_arrow_left',
-        },
-
-        pagination: {
-            el: '.otherProjects__dotted',
-        },
 
 
-    });
 
 
     function addSliders() {
@@ -476,50 +182,8 @@ $(document).ready(function() {
         });
 
     }
-    addSliders();
-
-    function addSteps() {
-
-        let steps = $('.steps-swiper-js');
-
-        steps.each(function() {
-            let options = $(this).data('options') || {};
-            let $parent = $(this).parent().parent();
-            let swiperDefaults = {
-
-                slidesPerView: 1,
-                spaceBetween: 10,
-                speed: 500,
-                loop: true,
-
-                pagination: {
-                    el: $parent.find('.steps__dotted')[0],
-                },
-
-                navigation: {
-                    nextEl: $parent.find('.icon_arrow_right')[0],
-                    prevEl: $parent.find('.icon_arrow_left')[0],
-                },
-
-                breakpoints: {
-                    768: {
-                        slidesPerView: 3,
-                        spaceBetween: 36,
-                    },
-                }
+    // addSliders();
 
 
-            };
-
-            let swiperOptions = $.extend(swiperDefaults, options),
-            mySwiper = new Swiper(this, swiperOptions);
-
-            // console.log($parent);
-            // console.log($parent.find('.steps__dotted')[0]);
-            // console.log($parent.closest('.projects__item').find('.projects-swiper-sm-js')[0]);
-        });
-
-    }
-    addSteps();
 
 });
